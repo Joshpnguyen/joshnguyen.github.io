a78f2bffccb6ffd387de8c9a044943a6a01f5a37
Joshpnguyen <Joshpnguyen@gmail.com>
2023-04-26 18:42:38 -0400

    Fixed My Resume button
    Added the ability to download my resume using the My Resume button

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,31 +1,23 @@
 import React from 'react';
-
 import { Section, SectionText, SectionTitle } from '../../styles/GlobalComponents';
 import Button from '../../styles/GlobalComponents/Button';
 import { LeftSection } from './HeroStyles';
-import FileSaver from 'file-saver';
+
 
 const Hero = (props) => {
-  const saveFile = () => {
-    FileSaver.saveAs(
-      "/resume.pdf",
-      "Josh Nguyen Resume.pdf"
-    );
-  }
-
   return(
   <Section row nopadding>
     <LeftSection>
       <SectionTitle>
         Hello there! <br />
-        I am Josh Nguyen
+        My name is Josh Nguyen
       </SectionTitle>
       <SectionText>
         I am an aspiring Software Engineer.
         <br></br>
         Click the link below to download my resume!
       </SectionText>
-      <Button onClick={saveFile}>Resume</Button>
+      <Button>My Resume</Button>
     </LeftSection>
   </Section>
   );
--- src/styles/GlobalComponents/Button.js
+++ src/styles/GlobalComponents/Button.js
@@ -1,11 +1,21 @@
-import React from 'react'
-
+import React from 'react';
+import { saveAs } from 'file-saver';
 import { ButtonBack, ButtonFront } from './index'
 
-const Button = (props) => (
-  <ButtonBack alt={props.alt} form={props.form} disabled={props.disabled}>{props.children}
-    <ButtonFront alt={props.alt} onClick={props.onClick} disabled={props.disabled}>{props.children}</ButtonFront>
-  </ButtonBack>
-);
+const Button = (props) => {
+  const handleDownload = async () => {
+    const fetchFile = await fetch('Resume.pdf');
+    const blobFromFetch = await fetchFile.blob();
+
+    const blob = new Blob([blobFromFetch], { type: 'application/pdf' });
+    saveAs(blob, 'josh_nguyen_resume.pdf');
+  }
+
+  return (
+    <ButtonBack alt={props.alt} form={props.form} disabled={props.disabled}>{props.children}
+    <ButtonFront alt={props.alt} onClick={handleDownload} disabled={props.disabled}>{props.children}</ButtonFront>
+    </ButtonBack>
+  );
+};
 
 export default Button
